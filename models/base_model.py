68329aace713f95060395712cbdb21ee87b8b390
Katrolli <ennio_cano@live.com>
2023-03-20 15:31:45 +0100

    testing alchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,24 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from models import storage
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.new(self)
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -29,16 +35,24 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
+        if self.__dict__['_sa_instance_method']:
+            del self.__dict__['_sa_instance_method']
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        for key, value in storage.all():
+            if value == self:
+                del storage.all()[key]
+                break
---
+++ models/engine/db_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import sqlalchemy
+from sqlalchemy import (create_engine)
+from models.base_model import BaseModel, Base
+import os
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+        metadata = MetaData(bind=self.__engine)
+
+        if HBNB_ENV == "test":
+            metadata.reflect(bind=self.__engine)
+            metadata.drop_all(bind=self.__engine)
+
+    def all(self, cls=None):
+        new_dict = {}
+        if cls != None:
+            all_obj = self.__session.query(cls).fetchall()
+        else:
+            all_obj = self.__session.query().fetchall()
+        for obj in all_obj:
+            index = obj.to_dict()['__class__'] + '.' + obj.id
+            new_dict[index] = obj
+        return new_dict
