1401e9529d793516f77684f9326ce7416907607d
FabioH7 <fhysollari4@gmail.com>
2023-03-20 16:08:08 +0100

    adding all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,6 +15,8 @@ class DBStorage:
         pwd = os.getenv('HBNB_MYSQL_PWD', default=None)
         host = os.getenv('HBNB_MYSQL_HOST', default=None)
         db = os.getenv('HBNB_MYSQL_DB', default=None)
+        print(user, pwd, host, db)
+        return
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
             user, pwd, host, db), pool_pre_ping=True)
 
