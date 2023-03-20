f3335d16c999eae9bd73aeff22e64bb1a452385f
Katrolli <ennio_cano@live.com>
2023-03-20 23:10:23 +0100

    adding relationship

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import sqlalchemy
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -11,3 +13,4 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state" cascade="delete")
