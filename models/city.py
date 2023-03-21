1f55af1f72ca8750662f665cc288ef792c923ca3
FabioH7 <fhysollari4@gmail.com>
2023-03-21 12:05:27 +0100

    adding all

--- models/city.py
+++ models/city.py
@@ -12,4 +12,4 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey(
         'states.id', ondelete="CASCADE"), nullable=False)
     name = Column(String(128), nullable=False)
-    state = relationship("State", back_populates="city")
+    state = relationship("State", back_populates="cities")
