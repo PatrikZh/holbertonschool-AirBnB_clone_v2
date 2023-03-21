f06cd4d0e4b27e00631e331842ddfb9a2ba8e007
FabioH7 <fhysollari4@gmail.com>
2023-03-21 15:05:28 +0100

    adding test

--- models/amenity.py
+++ models/amenity.py
@@ -4,11 +4,10 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
-        "Place", back_populates='amenities', secondary=place_amenity)
+        "Place", back_populates='amenities, place_amenity')
