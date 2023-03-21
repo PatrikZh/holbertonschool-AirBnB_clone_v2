98c686faadd8500032ac4b8df54cc77ffb377b4c
FabioH7 <fhysollari4@gmail.com>
2023-03-21 15:22:31 +0100

    adding test

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,11 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
-        "Place", back_populates='place_amenity')
+        "Place", secondary=place_amenity, back_populates='amenities')
--- models/place.py
+++ models/place.py
@@ -8,6 +8,15 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey, Table
 import os
 
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column("place_id", String(60), ForeignKey("places.id", ondelete="CASCADE"),
+           primary_key=True, nullable=False),
+    Column("amenity_id", String(60), ForeignKey("amenities.id", ondelete="CASCADE"),
+           primary_key=True, nullable=False)
+)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -29,15 +38,6 @@ class Place(BaseModel, Base):
     cities = relationship("City", back_populates='places')
     reviews = relationship(
         "Review", back_populates='places', cascade='all, delete-orphan')
-
-    place_amenity = Table(
-        "place_amenity",
-        Base.metadata,
-        Column("place_id", String(60), ForeignKey("places.id", ondelete="CASCADE"),
-               primary_key=True, nullable=False),
-        Column("amenity_id", String(60), ForeignKey("amenities.id", ondelete="CASCADE"),
-               primary_key=True, nullable=False)
-    )
 
     amenities = relationship(
         "Amenity", secondary=place_amenity, back_populates='place_amenities', viewonly=False)
