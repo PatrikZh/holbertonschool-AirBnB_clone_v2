d23069bfb14c78cf78f86ffe8b1207eae78a8e30
FabioH7 <fhysollari4@gmail.com>
2023-03-21 15:15:00 +0100

    adding test

--- models/place.py
+++ models/place.py
@@ -40,7 +40,7 @@ class Place(BaseModel, Base):
     )
 
     amenities = relationship(
-        "Amenity", secondary=place_amenity, back_populates='places', viewonly=False)
+        "Amenity", secondary=place_amenity, back_populates='place_amenities', viewonly=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
