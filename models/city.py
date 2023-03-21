228b098f68e3fbdddc4ce54b2eb159d13e555c27
FabioH7 <fhysollari4@gmail.com>
2023-03-21 13:35:47 +0100

    adding places and relationships

--- models/city.py
+++ models/city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import ForeignKey
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from models.base_model import BaseModel, Base
+0
+""" City Module for HBNB project """
 
 
 class City(BaseModel, Base):
@@ -13,3 +14,5 @@ class City(BaseModel, Base):
         'states.id', ondelete="CASCADE"), nullable=False)
     name = Column(String(128), nullable=False)
     state = relationship("State", back_populates="cities")
+    places = relationship('Place', back_populates='cities',
+                          cascade='all, delete-orphan')
--- models/place.py
+++ models/place.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    city_id = Column(String(60), ForeignKey(
+        'cities.id', ondelete="CASCADE"), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),
+                     nullable=False, ondelete='CASCADE')
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True, default=None)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True, default=None)
+    longitude = Column(Float, nullable=True, default=None)
     amenity_ids = []
+    user = relationship("User", back_populates='places')
+    cities = relationship("City", back_populates='places')
