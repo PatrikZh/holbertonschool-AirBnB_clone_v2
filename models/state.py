914e0b8f84ae3fd8e6cdfe0f832d8f56f5a29648
FabioH7 <fhysollari4@gmail.com>
2023-03-21 12:38:07 +0100

    adding all

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
@@ -16,10 +17,11 @@ class State(BaseModel, Base):
     cities = relationship("City", back_populates="state",
                           cascade="all, delete-orphan")
 
-    # @property
-    # def cities(self):
-    #     new_ls = []
-    #     for city in models.storage.all(City):
-    #         if self.id == city.state_id:
-    #             new_ls.append(city)
-    #     return new_ls
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            new_ls = []
+            for city in models.storage.all(City):
+                if self.id == city.state_id:
+                    new_ls.append(city)
+            return new_ls
