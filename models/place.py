2a024f22ce7c5f1bafad6f4a17b4fee4cf7ed039
FabioH7 <fhysollari4@gmail.com>
2023-03-21 18:13:52 +0100

    adding all

--- models/place.py
+++ models/place.py
@@ -11,9 +11,9 @@ import os
 place_amenity = Table(
     "place_amenity",
     Base.metadata,
-    Column("place_id", String(60), ForeignKey("places.id", ondelete="CASCADE"),
+    Column("place_id", String(60), ForeignKey("places.id"),
            primary_key=True, nullable=False),
-    Column("amenity_id", String(60), ForeignKey("amenities.id", ondelete="CASCADE"),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"),
            primary_key=True, nullable=False)
 )
 
@@ -40,7 +40,7 @@ class Place(BaseModel, Base):
         "Review", back_populates='places', cascade='all, delete-orphan')
 
     amenities = relationship(
-        "Amenity", secondary=place_amenity, back_populates='place_amenities', viewonly=False)
+        "Amenity", secondary='place_amenity', back_populates='place_amenities', viewonly=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
