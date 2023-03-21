27e46f4818dd0248aff6c67894e8c7d635286b4a
FabioH7 <fhysollari4@gmail.com>
2023-03-21 13:38:16 +0100

    adding places and relationships

--- models/place.py
+++ models/place.py
@@ -10,8 +10,8 @@ class Place(BaseModel):
     """ A place to stay """
     city_id = Column(String(60), ForeignKey(
         'cities.id', ondelete="CASCADE"), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'),
-                     nullable=False, ondelete='CASCADE')
+    user_id = Column(String(60), ForeignKey('users.id', ondelete='CASCADE'),
+                     nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True, default=None)
     number_rooms = Column(Integer, nullable=False, default=0)
