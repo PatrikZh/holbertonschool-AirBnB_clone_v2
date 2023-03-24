10f770a2b829c4d6ca0d35e585e7f33b775fc733
PatrikZh <pzheji@actirana.edu.al>
2023-03-24 14:17:10 +0100

    First Try

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,20 +1,27 @@
+
 #!/usr/bin/python3
-""" Module for testing DB storage"""
+""" Testing the console module """
+
 import unittest
 from models.engine import db_storage
 from models.engine.db_storage import DBStorage
 
-class Test_DBStorage(unittest.TestCase):
-    """ Module for testing DB storage"""
+
+class TestConsole(unittest.TestCase):
+    """ Test class for DBStorage """
+
     def test_documentation(self):
-        """ Module for testing DB storage"""
+        """ Test docstrings documentation"""
+
         self.assertTrue(db_storage.__doc__)
         self.assertTrue(db_storage.DBStorage.__doc__)
 
-    def test_method_doc(self):
-        """ Module for testing DB storage"""
-        for method in dir(DBStorage):
-            self.assertTrue(method.__doc__)
+    def test_methods_doc(self):
+        """ Test all docstrings of each method"""
 
-if __name__ == "__main__":
+        for all_methods in dir(DBStorage):
+            self.assertTrue(all_methods.__doc__)
+
+
+if __name__ == '__main__':
     unittest.main()
