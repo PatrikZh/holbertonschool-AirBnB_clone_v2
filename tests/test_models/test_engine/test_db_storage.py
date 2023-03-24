b9630b098ea5c8ec5aa3c36c57b89ccf15f2cd74
PatrikZh <pzheji@actirana.edu.al>
2023-03-24 14:00:26 +0100

    First Try

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -5,11 +5,13 @@ from models.engine import db_storage
 from models.engine.db_storage import DBStorage
 
 class Test_DBStorage(unittest.TestCase):
-    """Comment"""
+    """ Module for testing DB storage"""
     def test_documentation(self):
+        """ Module for testing DB storage"""
         self.assertTrue(db_storage.__doc__)
         self.assertTrue(db_storage.DBStorage.__doc__)
 
     def test_method_doc(self):
+        """ Module for testing DB storage"""
         for method in dir(DBStorage):
             self.assertTrue(method.__doc__)
