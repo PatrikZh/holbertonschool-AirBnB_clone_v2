71ea871071cec6ea88b82d97fe2e143e8a71cdef
FabioH7 <fhysollari4@gmail.com>
2023-04-03 14:27:01 +0200

    number export

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """ returns a string """
+    """ returns string """
     return render_template("5-number.html", n=n)
 
 
