f88e70dd146abf217c40fdb8b5dc7f42b2d03e40
FabioH7 <fhysollari4@gmail.com>
2023-04-03 14:24:47 +0200

    adding html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """ returns string """
+    """ returns a string """
     return render_template("5-number.html", n=n)
 
 
