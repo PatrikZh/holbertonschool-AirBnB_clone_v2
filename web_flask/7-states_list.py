84e31934b77170dd43dd03de055c171fbbba4408
FabioH7 <fhysollari4@gmail.com>
2023-04-04 12:49:44 +0200

    adding

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,6 @@ def teardown_storage(self):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all('State').values()
-    print(states)
     return render_template('7-states_list.html', states=states)
 
 
