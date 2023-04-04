7c5451feed24edfc8e6afe637965e97ccddbb999
FabioH7 <fhysollari4@gmail.com>
2023-04-04 13:17:16 +0200

    adding

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" python module """
 from models import storage
 from flask import Flask, render_template
 
@@ -6,11 +8,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_storage(self):
+    """ teardown context """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """ states list """
     states = storage.all('State').values()
     return render_template('7-states_list.html', states=states)
 
