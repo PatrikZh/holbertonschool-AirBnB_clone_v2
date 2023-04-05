e0b29d438e8d1e39d0a0e580ad8320dcc9c8a1a9
PatrikZh <patrik.zheji@gmail.com>
2023-04-05 04:23:49 -0700

    Adding another string

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Writing a script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' Returns string'''
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' Returns string'''
+    return "HBNB"
+
+if __name__ == '__main__':
+    ''' Checking if main file'''
+    app.run(host="0.0.0.0", port=5000, debug=True)
