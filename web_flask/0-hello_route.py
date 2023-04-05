464633380e340b2919e6c429b9917e4f47529c23
PatrikZh <patrik.zheji@gmail.com>
2023-04-05 04:19:29 -0700

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ Writing a script that starts a Flask web application"""
-
-
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     ''' Returns string'''
     return "Hello HBNB!"
+
+if __name__ == '__main__':
+    ''' Checking if main file'''
+    app.run(host="0.0.0.0", port=5000, debug=True)
