1cc61fba3bfc968134491a74acfc26854894cf7c
PatrikZh <patrik.zheji@gmail.com>
2023-04-07 02:45:38 -0700

    Update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,7 +23,7 @@ def c(text):
     modified_text = ""
     for i in range(len(text)):
         if text[i] == "_":
-            modified_text += " " + value
+            modified_text += " "
         else:
             modified_text += text[i]
     return value + " " + modified_text
