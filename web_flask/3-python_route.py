e2e07b486eaf7c79b9b69f461a691e297c9cfdab
PatrikZh <patrik.zheji@gmail.com>
2023-04-07 05:07:44 -0700

    First commit, adding another function to start an Flask web application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Writing a script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' Returns string'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' Returns string'''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    ''' Displaying modified string'''
+    value = "C"
+    modified_text = ""
+    for i in range(len(text)):
+        if text[i] == "_":
+            modified_text += " "
+        else:
+            modified_text += text[i]
+    return value + " " + modified_text
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    ''' Displaying modified string'''
+    value = "Python"
+    modified_text = ""
+    for i in range(len(text)):
+        if text[i] == "_":
+            modified_text += " "
+        else:
+            modified_text += text[i]
+    return value + " " + modified_text
+
+if __name__ == '__main__':
+    ''' Checking if main file'''
+    app.run(host="0.0.0.0", port=5000, debug=True)
