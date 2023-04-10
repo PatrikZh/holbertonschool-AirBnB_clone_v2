b8e74c7204e6566758383f4b54a292804f8be39e
PatrikZh <patrik.zheji@gmail.com>
2023-04-10 11:24:59 -0700

    Function to check if a number is even or odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -60,10 +60,7 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     ''' Checking number n is even or odd if its integer'''
-        if n % 2 == 0:
-            print("Number: {} is even".format(n))
-        else:
-            print("Number: {} is odd".format(n))
+    return render_template("6-number_odd_or_even.html", n=n)
 
 if __name__ == '__main__':
     ''' Checking if main file'''
