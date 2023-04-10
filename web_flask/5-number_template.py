4b2701d1a0d0367ec030c2980e28a13c126fecfb
PatrikZh <patrik.zheji@gmail.com>
2023-04-10 09:58:01 -0700

    Link flask framework with html using render_template() builtin function

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" Writing a script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' Returns string'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' Returns string'''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    ''' Displaying modified string'''
+    value = "C"
+    modified_text = ""
+    for i in range(len(text)):
+        if text[i] == "_":
+            modified_text += " "
+        else:
+            modified_text += text[i]
+    return value + " " + modified_text
+
+
+@app.route('/python', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def modify_text(text):
+    ''' Displaying modified string'''
+    value = "Python"
+    modified_text = ""
+    for i in range(len(text)):
+        if text[i] == "_":
+            modified_text += " "
+        else:
+            modified_text += text[i]
+    return value + " " + modified_text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_int(n):
+    ''' Adding only if number'''
+    modified_int = str(n)
+    message = "{} is a number".format(modified_int)
+    return message
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def number_template(n):
+    ''' HTML comes in only if n is an integer'''
+    return render_template("5-number.html", n=n)
+
+if __name__ == '__main__':
+    ''' Checking if main file'''
+    app.run(host="0.0.0.0", port=5000, debug=True)
